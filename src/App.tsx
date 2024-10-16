e2009f8316e9f21e3669af7980edf560ee6c4e1f
Golam Muntakim <seams123456789@gmail.com>
2024-10-15 19:25:39 -0700

    complete project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { incrementByValue, StateType } from "./redux"
+import { decrement, increment, incrementByValue, StateType } from "./redux"
 function App() {
   const [val, setVal] = useState<number>(0)
-  const  dispatch = useDispatch()
-  const count = useSelector((state:StateType)=>state.count)
+  const dispatch = useDispatch()
+  const count = useSelector((state: StateType) => state.count)
+  const incrementHandler = () =>{
+    dispatch(increment())
+  }
+  const decrementHandler = () =>{
+    dispatch(decrement())
+  }
 
- const incrementByValueHandler = () =>{
-  dispatch(incrementByValue(val))
- }
+  const incrementByValueHandler = () => {
+    setVal(0)
+    dispatch(incrementByValue(val))
+  }
 
   return (
     <>
       <div>
         <h1>Toolkit</h1>
         <h1>{count}</h1>
-        <button>+</button>
-        <button>-</button>
-        <input type="number" value={val} onChange={(e)=>setVal(Number(e.target.value))} />
-        <button disabled={val<0} onClick={incrementByValueHandler}>Add</button>
+        <button onClick={incrementHandler}>+</button>
+        <button disabled={count <= 0} onClick={decrementHandler}>-</button>
+        <input type="number" value={val} onChange={(e) => setVal(Number(e.target.value))} />
+        <button disabled={val < 0} onClick={incrementByValueHandler}>Add</button>
       </div>
     </>
   )
